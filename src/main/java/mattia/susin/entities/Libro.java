4dc841c046a5fdfa26629ac35d5f62dfa50176e7
MattiaSusin <m.susin28@gmail.com>
2024-08-23 11:17:52 +0200

    Update Libro.java

--- src/main/java/mattia/susin/entities/Libro.java
+++ src/main/java/mattia/susin/entities/Libro.java
@@ -8,6 +8,9 @@ import java.util.UUID;
 @Entity
 @Table(name = "libri")
 public class Libro {
+
+    //ATTRIBUTI
+
     @Id
     @GeneratedValue
     private UUID isbn;
@@ -26,5 +29,79 @@ public class Libro {
 
     @Column(name = "genere")
     private String genere;
-    
+
+    //COSTRUTTORI
+
+    public Libro() {
+        //COSTRUTTORE DI DEFAULT
+    }
+
+    public Libro(String titolo, Date annoPubblicazione, int numeroPagine, String autore, String genere) {
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numeroPagine = numeroPagine;
+        this.autore = autore;
+        this.genere = genere;
+    }
+
+    //SETTER GETTER
+
+    public UUID getIsbn() {
+        return isbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public Date getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(Date annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    //TO STRING
+
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "isbn=" + isbn +
+                ", titolo='" + titolo + '\'' +
+                ", annoPubblicazione=" + annoPubblicazione +
+                ", numeroPagine=" + numeroPagine +
+                ", autore='" + autore + '\'' +
+                ", genere='" + genere + '\'' +
+                '}';
+    }
 }
